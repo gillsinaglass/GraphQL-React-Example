946e5fad7766d1d6f06cb580005ec1267fd2959f
gillsinaglass <rdgill95@gmail.com>
2019-06-04 16:06:13 -0400

    GraphQL and MongoDB connected, Query visible

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 require('dotenv').config({path: 'variables.env'});
+const Recipe = require("./Models/Recipe");
+const User = require("./Models/User");
 
+// Bring in GraphQL Middleware
+const { graphiqlExpress, graphqlExpress} = require('apollo-server-express')
+const {makeExecutableSchema} = require('graphql-tools')
+
+const { typeDefs } = require ('./schema');
+const { resolvers } = require(`./resolvers`);
+
+// Create GraphQL Schema
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+// Connects to Database
 mongoose.connect(process.env.MONGO_URI)
     .then(()=> console.log('DB Connected'))
     .catch(err=> console.error(err));
 
+// Initializes Application
 const app = express();
 
+// Creates Graphiql application
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql'}
+))
+
+// Connect Schemas with GraphQL
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema,
+    context: {
+        Recipe,
+        User
+    }
+}))
 const PORT = process.env.PORT || 4444
 
 app.listen(PORT, () => {
