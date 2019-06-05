3f5e5e792e938d9352ec26876a00ca688437ab56
gillsinaglass <rdgill95@gmail.com>
2019-06-05 10:10:33 -0400

    BCrypt Password Protection enabled

--- Models/User.js
+++ Models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
     username: {
@@ -24,6 +25,24 @@ const UserSchema = new Schema({
         type: [Schema.Types.ObjectId],
         ref: 'Recipe'
     }
+});
+
+// before any user is saved to db
+UserSchema.pre('save', function(next) {
+    if (!this.isModified('password')) {
+        // call next function because we are not signing up user
+        return next();
+    }
+    // function to hash user password
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+        // 
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err);
+            this.password = hash;
+            next();
+        });
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema)
