3612241ca77bbc82fd4b9826310d1f3225824030
gillsinaglass <rdgill95@gmail.com>
2019-06-05 10:53:01 -0400

    SignUp Form Created

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -3,7 +3,18 @@ import React from 'react';
 class Signup extends React.Component {
     render() {
         return (
-            <div>Signup</div>
+            <div className="App">
+            
+                <h2 className="App">SignUp</h2>
+                <form className="form">
+                    <input type="text" name="username" placeholder="Username"/>
+                    <input type="email" name="email" placeholder="Email"/>
+                    <input type="password" name="password" placeholder="Password"/>
+                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password"/>
+                    <button type="submit" className="button-primary">Submit</button>
+                </form>
+            
+            </div>
         )
     }
 }
