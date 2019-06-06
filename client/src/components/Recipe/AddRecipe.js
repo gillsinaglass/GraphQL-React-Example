d72f445b71ca69757ff9d893dd19d516140c525e
gillsinaglass <rdgill95@gmail.com>
2019-06-06 09:32:54 -0400

    Add Recipe Form pre-mutation

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,30 +1,40 @@
 import React from 'react' 
 
 class AddRecipe extends React.Component{
-    state = {}
+    state = {
+        name: "",
+        instructions: "",
+        category: "Breakfast",
+        description: "",
+        username: ""
+    }
 
     handleChange = event => {
         const { name, value } = event.target
         console.log(name,":", value);
+        this.setState({
+            [name]: value
+        })
         
     }
 
 
 
     render() {
+        const { name, category, description, instructions} = this.state;
         return (
             <div className="App">
                 <h2 className="App">Add Recipe</h2>
                 <form className="form">
-                    <input type="text" name="name" placeholder="Recipe Name" onChange={this.handleChange} />
-                    <select name="category" onChange={this.handleChange}>
+                    <input type="text" name="name" placeholder="Recipe Name" onChange={this.handleChange} value={name}/>
+                    <select name="category" onChange={this.handleChange} value={category}>
                         <option value="Breakfast">Breakfast</option>
                         <option value="Lunch">Lunch</option>
                         <option value="Dinner">Dinner</option>
                         <option value="Snack">Snack</option>
                     </select>
-                    <input type="text" name="description" placeholder="Add description..." onChange={this.handleChange}/>
-                    <textarea name="instructions" placeholder="Add instructions..." onChange={this.handleChange}/>
+                    <input type="text" name="description" placeholder="Add description..." onChange={this.handleChange} value={description}/>
+                    <textarea name="instructions" placeholder="Add instructions..." onChange={this.handleChange} value={instructions}/>
                     <button type="submit" className="button-primary">Submit</button>
                 </form>
             </div>
