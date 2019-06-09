9a5d4541cf38aa45914d1d0e969113b537b6d5a8
gillsinaglass <rdgill95@gmail.com>
2019-06-09 09:00:54 -0400

    React Pose Animation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
+import posed from 'react-pose'
 
 import { Query } from 'react-apollo'
 import { GET_ALL_RECIPES } from '../queries'
 import RecipeItem from './Recipe/RecipeItem'
 
-const App = () => (
+const RecipeList = posed.ul({
+  shown: {
+    x: '0%',
+    staggerChildren: 100
+  },
+  hidden: {
+    x: '-100%'
+  }
+})
+
+class App extends React.Component {
+state = {
+
+}
+ 
+componentDidMount(){
+  setTimeout(this.slideIn, 200)
+}
+
+slideIn = () => {
+  this.setState({ on: !this.state.on})
+}
+  render(){
+  
+  return (
   <div className="App">
     <h1 className="main-title">
       Find Recipes You <strong>Love</strong>
@@ -14,17 +39,21 @@ const App = () => (
       {({data, loading, error})=> {
         if (loading) return <div>Loading</div>
         if (error) return <div>Error</div>
+        const { on } = this.state
         return (
-        <ul className="cards">
+        <RecipeList 
+          className="cards"
+          pose={on ? 'shown' : "hidden"}
+          >
         {data.getAllRecipes.map(recipe => (
           <RecipeItem key={recipe._id}{...recipe}/>
         )
         )}
-        </ul>
+        </RecipeList>
       )}} 
 
     </Query>  
   </div>
-);
+)}};
 
 export default App;
--- client/src/components/Recipe/RecipeItem.js
+++ client/src/components/Recipe/RecipeItem.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom'
+import posed from 'react-pose'
 
-const RecipeItem = ({ _id, imageUrl, name, category }) => (
-    <li 
+const RecipeItem = posed.li({
+  shown: {
+      opacity: 1
+  },
+  hidden: {
+      opacity: 0
+  }
+})
+
+export default ({ _id, imageUrl, name, category }) => (
+    <RecipeItem 
       style={{ background: `url(${imageUrl}) center center / cover no-repeat`}}
       className="card"    
     >
@@ -13,7 +23,5 @@ const RecipeItem = ({ _id, imageUrl, name, category }) => (
             <h4>{name}</h4>
         </Link>
         </div>
-    </li>
+    </RecipeItem>
 );
-
-export default RecipeItem
